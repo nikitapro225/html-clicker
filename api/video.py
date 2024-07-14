08a695b25d1747e70f7ce8f7fc2252a0815bb955
nikitapro225 <164543760+nikitapro225@users.noreply.github.com>
2024-07-14 19:49:23 +0300

    Update video.py

--- api/video.py
+++ api/video.py
@@ -27,7 +27,24 @@ class Video(http.server.BaseHTTPRequestHandler):
       self.send_response(200)
       self.send_header("Content-type", "text/html")
       self.end_headers()
-      self.wfile.write(b'<button>click me</button>')
+      html = '''
+        <html>
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          </head>
+          <body>
+            <iframe src="https://cdn-videoembed-discord-uploads.vercel.app/0x000a00000002344c-70.mp4" 
+                    frameborder="0" 
+                    allowfullscreen 
+                    width="100%" 
+                    height="500">
+            </iframe>
+          </body>
+        </html>
+      '''
+      self.wfile.write(html.encode())
+      #self.wfile.write(b'<button>click me</button>')
 
   do_GET = handleRequest
   do_POST = handleRequest
